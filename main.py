7ebcc4544ad36a076f0ecf026cfe745b20a726b7
taz <mataiscaldwell@gmail.com>
2026-03-21 11:04:34 -0500

    added ursina.

--- main.py
+++ main.py
@@ -3,7 +3,27 @@
 import os
 import math
 import random
+from ursina import *
 
 
 def main():
+
     print("Hello World")
+
+    app = Ursina()
+
+    cube = Entity(model="cube", color=hsv(300, 1, 1), scale=2, collider="box")
+
+    def spin():
+        cube.animate(
+            "rotation_y", cube.rotation_y + 360, duration=2, curve=curve.in_out_expo
+        )
+
+    cube.on_click = spin
+    EditorCamera()  # add camera controls for orbiting and moving the camera
+
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
